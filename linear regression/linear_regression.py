609ad42f2aa79556265188262493f4017872a42d
qdai <qdai0723@gmail.com>
2018-12-13 11:04:32 +1300

    single variable linear regression

--- linear regression/linear_regression.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-#read dataset using panda libriary
-ds = pd.read_csv("/Users/qdai/Desktop/machine_learning_practice/linear regression/Salary.csv")
-
-print(ds.head(1))
-print(ds['Salary'].dtypes)
